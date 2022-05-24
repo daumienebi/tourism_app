b14f6b1487b41bc8fa82002a6af380c0b2f7b502
daumienebi <ddsakpa@gmail.com>
2022-05-24 23:06:14 +0200

    Visit place detail page and few map changes

--- lib/src/widgets/my_searchbar.dart
+++ lib/src/widgets/my_searchbar.dart
@@ -5,18 +5,8 @@ import 'package:tourisim_app/src/providers/visitplace_provider.dart';
 
 class MySearchBar extends SearchDelegate{
 
-
-
   @override
   List<Widget>? buildActions(BuildContext context) {
-    Consumer(
-        builder: (_, VisitPlaceProvider provider, __) {
-          List<String> suggestions = provider.suggestions;
-          return
-              Container();
-        }
-    );
-
     return [
     IconButton(
         onPressed: (){
@@ -43,10 +33,17 @@ class MySearchBar extends SearchDelegate{
     // TODO: implement buildResults
     VisitPlaceProvider provider = Provider.of<VisitPlaceProvider>(context,listen :false);
     //get visit place names from the db
-    //List<String> suggestions = provider.suggestions;
-    return Center(
-        child: Text(query),
-    );
+    List<VisitPlace> suggestions = provider.placesList;
+
+    final suggestionList = query.isEmpty
+        ? []
+        : suggestions.where((p) => p.name.contains(query)).toList();
+    VisitPlace place = suggestionList[0];
+    print(suggestionList.length);
+    print(place.toString());
+    return SingleChildScrollView(child: DetailHead(place: place));
+
+
   }
 
   @override
@@ -55,25 +52,24 @@ class MySearchBar extends SearchDelegate{
     //get visit place names from the db
     List<VisitPlace> placesList = provider.placesList;
 
+    List<VisitPlace> suggestions = placesList.where((visitPlace){
 
-    List<String> searchResults = provider.suggestions;
-    List<String> suggestions = searchResults.where((searchResult){
-      final result = searchResult.toLowerCase();
-      final input = query.toLowerCase();
+      final visitPlaceName = visitPlace.name.toLowerCase();
+      final userInput  = query.toLowerCase();
 
-      return result.contains(input);
+      return visitPlaceName.contains(userInput);
     }).toList();
           return FutureBuilder(
-              future: provider.getSuggestions(),
+              future: provider.getVisitPlaces(),
               builder: (_,AsyncSnapshot data){
                 return ListView.separated(
                     itemBuilder: (_,pos){
                       final suggestion = suggestions[pos];
                       return ListTile(
                         leading: Icon(Icons.place,color: Colors.green,size: 35,),
-                        title: Text(suggestion) ,
+                        title: Text(suggestion.name) ,
                         onTap: (){
-                          query =  suggestion;
+                          query =  suggestion.name;
                           showResults(context);
                         },
                       );
@@ -84,5 +80,103 @@ class MySearchBar extends SearchDelegate{
               }
           );
         }
+
+}
+
+class DetailHead extends StatelessWidget{
+  VisitPlace place;
+  DetailHead({required this.place});
+   //final controller = TextEditingController(text: place.description);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Column(
+        children: [
+          _details(context)
+        ],
+      ),
+    );
+  }
+
+
+  Widget _details(context){
+
+    return Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            height: 200,
+            width : 200,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Card(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: Image.asset('assets/images/damian.jpg',fit: BoxFit.cover,)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: TextFormField(
+              //controller: controller,
+              initialValue: place.description.isEmpty ? 'No Description' : place.description,
+              maxLines: null,
+              enabled: false,
+              decoration: InputDecoration(
+                label: Text('Description' ,style: TextStyle(color: Colors.cyan,fontSize: 22),)
+              ),
+              style: TextStyle(fontSize: 17),
+            )
+          ),
+          Container(
+            margin:EdgeInsets.only(top: 20,bottom: 10),
+            child: ListTile(
+              title: Text("Name: ",style: TextStyle(fontSize: 17,color: Colors.white),),
+              tileColor: Colors.black87,
+              leading: Icon(Icons.place_outlined,color: Colors.white,),
+              trailing: Text(place.name.toUpperCase(),style: TextStyle(fontSize: 17,color: Colors.white,fontWeight: FontWeight.bold)),
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: ListTile(
+                title: Text("Latitude :",style: TextStyle(fontSize: 17,color: Colors.white),),
+                tileColor: Colors.black87,
+                leading: Icon(Icons.map,color: Colors.white,),
+                trailing: Text(place.latitude,style: TextStyle(fontSize: 15,color: Colors.white,fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: ListTile(
+                title: Text("Longitude :",style: TextStyle(fontSize: 17,color: Colors.white),),
+                tileColor: Colors.black87,
+                leading: Icon(Icons.map,color: Colors.white,),
+                trailing: Text(place.longitude,style: TextStyle(fontSize: 15,color: Colors.white,fontWeight: FontWeight.bold)),
+            ),
+          ),
+
+          TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text("Close",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red,
+              fixedSize: Size(100,40),
+            ),
+          )
+          /*
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+          child: Text(mascota.desc, style: TextStyle(fontSize: 18,overflow: TextOverflow.visible ),)
+          ),
+        */
+        ]
+    );
+  }
+
 
 }
